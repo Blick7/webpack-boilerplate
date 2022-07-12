2ed173ecd5d79e63371dc22e936c4b2ffe215417
Blick <45530302+Blick7@users.noreply.github.com>
2022-07-12 15:02:02 +0300

    setup webpack boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+let mode = 'development';
+let target = 'web';
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production';
+    target = 'browserslist'; // use browserslist in production
+}
+
+const plugins = [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+    }),
+    new ESLintPlugin(),
+];
+
+module.exports = {
+    mode,
+    target,
+    plugins,
+    entry: {
+        main: path.resolve(__dirname, './src/index.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: '[hash][ext][query]',
+        filename: '[name].bundle.js',
+        clean: {
+            keep: /\.git/,
+        },
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.ts'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+                type: mode === 'production' ? 'asset' : 'asset/resource',
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
